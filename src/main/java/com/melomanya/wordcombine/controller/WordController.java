b6286af46f2f688d36b93df2b1f3238622d57f18
Melih Basayigit <melihbasayigit@gmail.com>
2023-03-19 03:38:10 +0300

    db properties updated

--- src/main/java/com/melomanya/wordcombine/controller/WordController.java
+++ src/main/java/com/melomanya/wordcombine/controller/WordController.java
@@ -4,6 +4,7 @@ import com.melomanya.wordcombine.model.WordList;
 import com.melomanya.wordcombine.service.WordService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -46,9 +47,50 @@ public class WordController {
     }
 
     private String processWords(WordList wordList) {
+        double similarityRate = 0.249999f;
 
         return "result";
     }
+
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("The weather is the most important subject in the land. In Europe, \n" +
+                "people say, ‘He is the type of person who talks about the \n" +
+                "weather,’ to show that somebody is very boring. In England, the \n" +
+                "weather is always an interesting, exciting subject and you must be \n" +
+                "good at talking about it.");
+        list.add("is always an interesting, exciting subject and you must be \n" +
+                "good at talking about it.\n" +
+                "\n" +
+                "George Mikes wrote this book to tell the English what he thought \n" +
+                "about them. He is both funny and rude about the strange things \n" +
+                "English people do and say - the things that make them different \n" +
+                "from other Europeans. In this book you will learn many useful \n" +
+                "rules about being English.");
+        list.add("George Mikes wrote this book to tell the English what he thought \n" +
+                "about them. He is both funny and rude about the strange things \n" +
+                "English people do and say - the things that make them different \n" +
+                "from other Europeans. In this book you will learn many useful \n" +
+                "rules about being English. You will learn how to talk about the \n" +
+                "weather, and what to say when somebody brings you a cup of tea \n" +
+                "at 5 o’clock in the morning. You will discover what the English \n" +
+                "really think of clever people and doctors. ");
+        list.add("You will discover what the English \n" +
+                "really think of clever people and doctors. This book will help you \n" +
+                "to be more like the English. As George Mikes says: ‘If you are \n" +
+                "like the English, they think you are funny. If you are not like \n" +
+                "them, they think you are even funnier.’\n" +
+                "\n" +
+                "George Mikes was born in Hungary in 1921. He studied law at \n" +
+                "Budapest University, and then began to write for newspapers. He \n" +
+                "came to London for two weeks just before the Second World War \n" +
+                "began, and made England his home for the rest of his life. During \n" +
+                "the war he worked for the BBC, making radio programmes for \n" +
+                "Hungary.");
+
+
+    }
+
 
 
 
